9d0e839209607ef183d218f581fe169e9b6e2835
david <david.ben.freeman@gmail.com>
2025-05-05 14:57:12 +0200

    added send text data to server function

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -3,4 +3,21 @@ async function getStatistics() {
     const count = await response.json()
     console.log(count)
 }
+
+function sendData() {
+    const txt = document.querySelector('#TxtInput').value
+    fetch('http://localhost:5229/text/', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json', },
+        body: JSON.stringify({
+            text: txt
+        })
+    }).then(resp => {
+        console.log('Response: ', resp)
+        if (resp.status === 200) {
+            getStatistics()
+        }
+    }).catch(error => console.log(error))
+}
+
 getStatistics()
