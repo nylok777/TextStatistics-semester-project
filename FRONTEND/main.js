3f7b290716f30444a686e6beb7c49574327af970
david <david.ben.freeman@gmail.com>
2025-05-06 10:15:48 +0200

    table prototype done

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,7 +1,38 @@
+let statsArray = []
+
 async function getStatistics() {
     const response = await fetch('http://localhost:5229/text')
-    const count = await response.json()
-    console.log(count)
+    const statistics = await response.json()
+    console.log(statistics)
+
+    const statTable = document.querySelector('#stat-table')
+
+    const tr = document.createElement('tr')
+    const charCount = document.createElement('td')
+    const wordCount = document.createElement('td')
+    const sentenceCount = document.createElement('td')
+    const mostComWord = document.createElement('td')
+    const readIndex = document.createElement('td')
+
+    charCount.innerHTML = statistics.charCount
+    wordCount.innerHTML = statistics.wordCount
+    sentenceCount.innerHTML = statistics.sentenceCount
+    mostComWord.innerHTML = statistics.mostComWord
+    readIndex.innerHTML = statistics.readIndex
+
+    charCount.classList.add('text-center')
+    wordCount.classList.add('text-center')
+    sentenceCount.classList.add('text-center')
+    mostComWord.classList.add('text-center')
+    readIndex.classList.add('text-center')
+
+    tr.appendChild(charCount)
+    tr.appendChild(wordCount)
+    tr.appendChild(sentenceCount)
+    tr.appendChild(mostComWord)
+    tr.appendChild(readIndex)
+    
+    statTable.appendChild(tr)
 }
 
 function sendData() {
