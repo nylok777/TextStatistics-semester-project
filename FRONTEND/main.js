d2ebae6a543b42bdca4d0d654d25d94ddbcc6545
nylok777 <david.ben.freeman@gmail.com>
2025-05-10 16:34:57 +0200

    done: switch to results page

--- FRONTEND/main.js
+++ FRONTEND/main.js
@@ -1,61 +1,6 @@
-let headsArray = ['Karakterek száma', 'Szavak száma', 'Mondatok száma', 'Leggyakoribb szó', 'ARI olvashatósági index']
-let headerCreated = false
-
 // window.onload = function() {
 //     document.getElementById('TxtInput').value = ''
 // }
-
-async function getStatistics() {
-    
-    
-    window.open('results.html', '_top')
-    const response = await fetch('http://localhost:5229/text')
-    const statistics = await response.json()
-    console.log(statistics)
-    window.addEventListener('DOMContentLoaded', () => {
-        const statTable = document.querySelector('#stat-table')
-
-        const statTableHead = document.querySelector('#stat-table-head')
-        if (headerCreated == false) {
-            headsArray.forEach(x => {
-                let th = document.createElement('th')
-                th.innerHTML = x
-                statTableHead.appendChild(th)
-            })
-            headerCreated = true
-        }
-        
-
-        const tr = document.createElement('tr')
-        const charCount = document.createElement('td')
-        const wordCount = document.createElement('td')
-        const sentenceCount = document.createElement('td')
-        const mostComWord = document.createElement('td')
-        const readIndex = document.createElement('td')
-
-        charCount.innerHTML = statistics.charCount
-        wordCount.innerHTML = statistics.wordCount
-        sentenceCount.innerHTML = statistics.sentenceCount
-        mostComWord.innerHTML = statistics.mostComWords[0]
-        readIndex.innerHTML = Math.round(statistics.readIndex)
-
-        charCount.classList.add('text-center')
-        wordCount.classList.add('text-center')
-        sentenceCount.classList.add('text-center')
-        mostComWord.classList.add('text-center')
-        readIndex.classList.add('text-center')
-
-        tr.appendChild(charCount)
-        tr.appendChild(wordCount)
-        tr.appendChild(sentenceCount)
-        tr.appendChild(mostComWord)
-        tr.appendChild(readIndex)
-        
-        statTable.appendChild(tr)
-
-        const mostComWordsCounts = statistics.mostComWordCounts    
-    })
-}
 
 function sendData() {
     let txt = document.querySelector('#TxtInput').value
@@ -67,7 +12,7 @@ function sendData() {
     }).then(resp => {
         console.log('Response: ', resp)
         if (resp.status === 200) {
-            getStatistics()
+            window.location.replace('results.html')
         }
     }).catch(error => console.log(error))
     document.getElementById('TxtInput').value = ''
---
+++ FRONTEND/results.js
@@ -0,0 +1,53 @@
+let headsArray = ['Karakterek száma', 'Szavak száma', 'Mondatok száma', 'Leggyakoribb szó', 'ARI olvashatósági index']
+let headerCreated = false
+
+
+async function getStatistics() {
+    const response = await fetch('http://localhost:5229/text')
+    const statistics = await response.json()
+    console.log(statistics)
+    
+    const statTable = document.querySelector('#stat-table')
+
+    const statTableHead = document.querySelector('#stat-table-head')
+    if (headerCreated == false) {
+        headsArray.forEach(x => {
+            let th = document.createElement('th')
+            th.innerHTML = x
+            statTableHead.appendChild(th)
+        })
+        headerCreated = true
+    }
+    
+
+    const tr = document.createElement('tr')
+    const charCount = document.createElement('td')
+    const wordCount = document.createElement('td')
+    const sentenceCount = document.createElement('td')
+    const mostComWord = document.createElement('td')
+    const readIndex = document.createElement('td')
+
+    charCount.innerHTML = statistics.charCount
+    wordCount.innerHTML = statistics.wordCount
+    sentenceCount.innerHTML = statistics.sentenceCount
+    mostComWord.innerHTML = statistics.mostComWords[0]
+    readIndex.innerHTML = Math.round(statistics.readIndex)
+
+    charCount.classList.add('text-center')
+    wordCount.classList.add('text-center')
+    sentenceCount.classList.add('text-center')
+    mostComWord.classList.add('text-center')
+    readIndex.classList.add('text-center')
+
+    tr.appendChild(charCount)
+    tr.appendChild(wordCount)
+    tr.appendChild(sentenceCount)
+    tr.appendChild(mostComWord)
+    tr.appendChild(readIndex)
+    
+    statTable.appendChild(tr)
+
+    const mostComWordsCounts = statistics.mostComWordCounts    
+}
+
+window.onload(getStatistics())
